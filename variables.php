639fd868dacd35d8c634065c5f5221c285d7344a
Michael Godel <mikegodel366@gmail.com>
2026-01-27 10:02:58 -0700

    Add files via upload

---
+++ variables.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">    
+        <title>VAriables</title>
+    </head>
+    <body>
+        <?php
+            $first_name = "Michael";
+            $last_name = "Godel";
+            $address = "123 Cool Street";
+            $phone_number = 9281234567;
+            
+            print "<p>My first name is: $first_name<br>My last name is: $last_name<br>My address is: $address<br>My phone number is: $phone_number</p>"
+        ?>
+    </body>
+</html>
