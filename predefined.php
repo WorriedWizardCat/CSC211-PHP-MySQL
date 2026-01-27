8cbb6d89e81adf91733d6974caac1b2d86b26adb
Michael Godel <mikegodel366@gmail.com>
2026-01-27 08:26:44 -0700

    Add files via upload

---
+++ predefined.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Server Info</title>
+    </head>
+    <body>
+        <pre>
+            <?php print_r($_SERVER)?>
+        </pre>
+    </body>
+</html>
