29852f2fc52e362a0c2a79021db686ce3d12c96a
Michael Godel <mikegodel366@gmail.com>
2026-01-29 10:26:39 -0700

    Add files via upload

---
+++ handle_form.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Your Feedback</title>
+    </head>
+    <body>
+        <?php
+            $title = $_POST["title"];
+            $first_name = $_POST["first_name"];
+            $last_name = $_POST["last_name"];
+            $email = $_POST["email"];
+            $response = $_POST["response"];
+            $comments = $_POST["comments"];
+
+            print "<p>Thank you, $title $first_name $last_name, for submitting your feedback. You used the following email to submit this feedback: $email</p> 
+			<p>You stated that you found this example to be '$response' and added: <br>$comments</p>";
+        ?>
+    </body>
+</html>
