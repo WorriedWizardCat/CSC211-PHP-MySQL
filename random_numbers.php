c60b6e7f31a71340b508301ac7b2543bd0049d4f
Michael Godel <mikegodel366@gmail.com>
2026-02-04 11:19:58 -0700

    Add files via upload

---
+++ random_numbers.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Radnom Numbers</title>
+    </head>
+    <body>
+        <?php
+            echo(rand(10, 100));
+        ?>
+    </body>
+</html>
