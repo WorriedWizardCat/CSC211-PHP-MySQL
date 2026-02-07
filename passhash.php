6807e2a1396fc3610b87d704333228013ccd7e42
Michael Godel <mikegodel366@gmail.com>
2026-02-07 08:52:49 -0700

    Add files via upload

---
+++ passhash.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Pass Hash</title>
+    </head>
+    <body>
+        <?php
+
+            $password = "helloclass";
+
+            $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+            echo  $hashed_password;
+
+        ?>
+    </body>
+</html>
