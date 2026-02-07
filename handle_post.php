5492b62aa8e438a210f05b998b5afa7a8bf90727
Michael Godel <mikegodel366@gmail.com>
2026-02-07 07:52:27 -0700

    Add files via upload

--- handle_post.php
+++ handle_post.php
@@ -9,7 +9,7 @@
             $first_name = $_POST["first_name"];
             $last_name = $_POST["last_name"];
             $email = $_POST["email"];
-            $posting = nl2br($_POST["posting"], false);
+            $posting = $_POST["posting"];
             $name = $first_name ." ". $last_name;
 
             print"<div>Thank you, $name, for your posting<p>$posting</P></div>";
