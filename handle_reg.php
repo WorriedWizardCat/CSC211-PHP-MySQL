f5f67345997bbf74b983a57afb9801d4033bf01c
Michael Godel <mikegodel366@gmail.com>
2026-02-11 09:30:04 -0700

    Add files via upload

---
+++ handle_reg.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Registration Form</title>
+    </head>
+    <body>
+        <h1>Registration Results</h1>
+        <?php
+            ini_set("display_errors",0);
+            error_reporting(E_ALL);
+            
+            $okay = true;
+            if ($okay) {
+                print "<p>You have been successfully registered (but not really)</p>";
+            }
+        ?>
+    </body>
+</html>
