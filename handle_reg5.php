cb6867bebaa43441ccad7ce96725f600b88db003
Michael Godel <mikegodel366@gmail.com>
2026-02-14 15:32:33 -0700

    Add files via upload

---
+++ handle_reg5.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Registration Form</title>
+        <style type="text/css" media="screen">
+            .error {color: red;}
+        </style>
+    </head>
+    <body>
+        <h1>Registration Results</h1>
+        <?php
+            ini_set("display_errors",0);
+            error_reporting(E_ALL);
+            
+            $okay = true;
+
+             if (empty($_POST["password"])) {
+                print "<p class='error'>Please enter your password.</p>";
+                $okay = false;
+            }
+
+            if($_POST["password"] != $_POST["confirm"]) {
+                print "<p class='error'>Your confirmed password does not match the original password.</p>";
+                $okay = false;
+            }
+            
+            if (is_numeric($_POST["year"]) AND (strlen($_POST["year"]) == 4) ) {
+                if ($_POST["year"] < 2026) {
+                    $age = 2026 - $_POST["year"];
+                } else {
+                    print "<p class='error'>Either you entered your birht year wrong or you come from the future!</p>";
+                    $okay = false;
+                }
+            } else {
+                print "<p class='error'>Please enter the year you were born as four digits.</p>";
+                $okay = false;
+            }
+
+            if (!isset($_POST['terms']) AND ($_POST['terms'] != 'Yes')) {
+                print '<p class="error">You must accept the terms.</p>';
+                $okay = FALSE;
+            }
+
+            if ($okay) {
+                print "<p>You have been successfully registered (but not really)</p>";
+                print "<p>You will be turning $age this year.";
+            }
+        ?>
+    </body>
+</html>
