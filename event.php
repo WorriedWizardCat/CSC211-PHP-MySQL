ae7060293b26b7597a983a312a5a8a33afc6662f
Michael Godel <mikegodel366@gmail.com>
2026-02-15 19:06:41 -0700

    Add files via upload

---
+++ event.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Add An Event</title>
+    </head>
+    <body>
+        <?php
+            print "<p>You want to add an event called <b>{$_POST['name']}</b> which takes place on: <br>";
+            if (isset($_POST["days"]) AND is_array($_POST["days"])){
+                foreach ($_POST["days"] as $day){
+                    print "$day<br>\n";
+                }
+            } else {
+                print "Please select at least one weekday for this event!";
+            }
+            print "</p>";
+        ?>
+    </body>
+</html>
