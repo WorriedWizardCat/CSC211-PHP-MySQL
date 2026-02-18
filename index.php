10c5c9851147bbfd1030a4015401bfbbd506ddf0
Michael Godel <mikegodel366@gmail.com>
2026-02-18 09:41:00 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+    ini_set("display_errors",1);
+    error_reporting(E_ALL);
+
+    include("templates/header.html");
+?>
+<h2>Welcome to a J.D. Salinger Fan Club</h2>
+<p>Lorem ipsum dolor sit amet...</p>
+<?php
+include("templates/footer.html");
+?>
