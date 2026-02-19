66d92a7d068e08973d78f268df7e8aef152b6a5a
Michael Godel <mikegodel366@gmail.com>
2026-02-19 08:31:09 -0700

    Add files via upload

--- books.php
+++ books.php
@@ -1,33 +1,14 @@
-<DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <title>Larry Ullman's Books and Chapters</title>
-    </head>
-    <body>
-        <h1>Some of Larry Ullman's Books</h1>
-        <?php
-            $phpvqs = [1 => "Getting Started With PHP", "Variables", "HTML Forms and PHP", "Using Numbers"];
-            $phpadv = [1 => 'Advanced PHP Techniques', 'Developing Web Applications', 'Advanced Database Concepts', 'Basic Object-Oriented Programming'];
-            $phpmysql = [1 => 'Introduction to PHP', 'Programming with PHP', 'Creating Dynamic Web Sites', 'Introduction to MySQL'];
-
-            $books = [
-                "PHP VQS" => $phpvqs,
-                "PHP Advanced VQP" => $phpadv,
-                "PHP and MySQL VQP" => $phpmysql
-            ];
-
-            print "<p>The third chapter of my first book is <i>{$books['PHP VQS'][3]}</i>.</p>";
-            print "<p>The first chapter of my second book is <i>{$books['PHP Advanced VQP'][1]}</i>.</p>";
-            print "<p>The fourth chapter of my fourth book is <i>{$books['PHP and MySQL VQP'][4]}</i>.</p>";
-
-            foreach ($books as $title => $chapters) {
-                print "<p>$title";
-                foreach ($chapters as $number => $chapter) {
-                    print "<br />Chapter $number is $chapter";
-                }
-                print "</p>";
-            }
-        ?>
-    </body>
-</html>
+<?php
+    define("TITLE","Books by J.D. Salinger");
+    include("templates/header.html");
+?>
+<h2>J.D Salinger's Books</h2>
+<ul>
+    <li>The Catcher in the Rye</li>
+    <li>Nine Stories</li>
+    <li>Franny and Zooey</li>
+    <li>Raise High the Roof Beam, Carpenters and Seymour: An Introduction</li>
+</ul>
+<?php
+    include("templates/footer.html");
+?>
