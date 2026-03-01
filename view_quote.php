a27f074b7101f867b7f48cf8d265aedb2fa91c11
Michael Godel <mikegodel366@gmail.com>
2026-03-01 09:10:02 -0700

    Add files via upload

---
+++ view_quote.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>View A Quotation</title>
+    </head>
+    <body>
+        <h1>Random Quotation</h1>
+        <?php
+            $data = file("quotes.txt");
+            $n = count($data);
+            $rand = rand(0, ($n - 1));
+            print "<p>" . trim($data[$rand]) . "</p>";
+        ?>
+    </body>
+</html>
