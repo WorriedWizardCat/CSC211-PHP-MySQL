f1b35ee8439c6a9ea2a28176db89cca5f5c0e888
Michael Godel <mikegodel366@gmail.com>
2026-03-02 14:53:34 -0700

    Add files via upload

--- register.php
+++ register.php
@@ -1,50 +1,61 @@
-<?php
-    define('TITLE', 'Register');
-    include('templates/header.html');
-    print '<h2>Registration Form</h2> <p>Register so that you can take advantage of certain features like this, that, and the other thing. </p>';
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $problem = false;
-        if (empty($_POST['first_name'])) {
-            $problem = true;
-            print '<p class="text--error">Please enter your first name!</p>';
-        }
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Register</title>
+        <style type="text/css" media="screen">
+            .error {
+                color: red
+            }
 
-        if (empty($_POST['last_name'])) {
-            $problem = true;
-            print '<p class="text--error">Please enter your last name!</p>';
-        }
-        
-        if (empty($_POST['email']) || (substr_count($_POST['email'], '@') != 1)) {
-            $problem = true;
-            print "<p class='text--error'>Please enter your email address!</p>";
-        }
+        </style>
+    </head>
+    <body>
+        <h1>Register</h1>
+        <?php
+            $dir = "users/";
+            $file = $dir . "users.txt";
+            $password2 = 123456;
 
-        if (empty($_POST["password1"])) {
-            $problem = true;
-            print "'<p class='text--error'>Please enter a password!</p>";
-        }
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $problem = FALSE;
+                if (empty($_POST["username"])) {
+                    $problem = TRUE;
+                    print "<p class='error'>Please enter a username!</p>";
+                }
 
-        if ($_POST["password1"] != $_POST["password2"]) {
-            $problem = true;
-            print "<p class='text--error'>Your password did not match your confirmed password!</p>";
-        }
+                if (empty($_POST["password1"])) {
+                    $problem = TRUE;
+                    print "<p class='error'>Please enter a password!</p>";
+                }
 
-        if (!$problem) {
-            print "<p class='text--success'>You are now registered!<br>Okay, you are not really registered but...</p>";
-            $body = "Thank you, {$_POST['first_name']}, for registering with the J.D. Salinger fan club!.";
-            mail($_POST['email'], 'Registration Confirmation', $body, 'From: admin@example.com');
-            $_POST = [];
-        } else {
-            print'<p class="text--error">Please try again!</p>';
-        }
-    }
-?>
-<form action="register.php" method="post" class="form--inline">
-    <p><label for="first_name">First Name:</label><input type="text" name="first_name" size="20" value="<?php if (isset($_POST['first_name'])) { print htmlspecialchars($_POST['first_name']); } ?>"></p>
-    <p><label for="last_name">Last Name:</label><input type="text" name="last_name" size="20" value="<?php if (isset($_POST['last_name'])) { print htmlspecialchars($_POST['last_name']); } ?>"></p>
-    <p><label for="email">Email Address:</label><input type="email" name="email" size="20" value="<?php if (isset($_POST['email'])) { print htmlspecialchars($_POST['email']); } ?>"></p>
-    <p><label for="password1">Password:</label><input type="password" name="password1" size="20" value="<?php if (isset($_POST['password1'])) { print htmlspecialchars($_POST['password1']); } ?>"></p>
-    <p><label for="password2">Confirm Password:</label><input type="password" name="password2" size="20" value="<?php if (isset($_POST['password2'])) { print htmlspecialchars($_POST['password2']); } ?>"></p>
-    <p><input type="submit" name="submit" value="Register!" class="button--pill"></p>
-</form>
-<?php include('templates/footer.html');?>
+                if ($_POST["password1"] != $password2) {
+                    $problem = TRUE;
+                    print "<p class='error'>Your password does not match your confirmed password!</p>";
+                }
+
+                if(!$problem) {
+                    if (is_writable($file)) {
+                        $subdir = time() . rand(0, 4596);
+                        $data = $_POST["username"] . "\t" . sha1(trim($_POST["password1"]) . "\t" . $subdir . PHP_EOL);
+                        file_put_contents($file, $data, FILE_APPEND | LOCK_EX);
+                        mkdir($dir . "MY_DIRECTORY_" . $subdir);
+                        print "<p>You are now registered!</p>";
+                    } else {
+                        print "<p class='error'>You could not be registered due to a system error.</p>";
+                    }
+                } else {
+                    print "<p class='error'>Please go back and try again!</p>";
+                }
+            } else {
+        ?>
+        <form action="" method="post">
+            <p>Username: <input type="text" name="username" size="20"></p>
+            <p>Password: <input type="password" name="password1" size="20"></p>
+            <input type="submit" name="submit" value="Login">
+        </form>
+        <?php
+            }
+        ?>
+    </body>
+</html>
